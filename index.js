fb150deee7fe35617a840ef478c77d160ba91d70
Your Name <julioazadte@gmail.com>
2024-06-16 18:52:03 -0300

    test

--- index.js
+++ index.js
@@ -7,17 +7,37 @@ const PORT = 3000;
 
 app.get('/', async (req, res) => {
     try {
-        const response = await axios.get('https://www.youtube.com');
+        const videoUrl = 'https://www.youtube.com/@todonoticias/streams';
+        const response = await axios.get(videoUrl);
         const html = response.data;
         const $ = cheerio.load(html);
 
-        // Ejemplo de scraping: obtener títulos de videos
-        let videoTitles = [];
-        $('a#video-title').each((index, element) => {
-            videoTitles.push($(element).text());
-        });
+        // Obtener título del video
+        const title = $('meta[name="title"]').attr('content') || $('title').text();
 
-        res.json(videoTitles);
+        // Obtener descripción del video
+        const description = $('meta[name="description"]').attr('content') || $('meta[property="og:description"]').attr('content');
+
+        // Obtener número de vistas del video
+        const viewsText = $('meta[itemprop="interactionCount"]').attr('content');
+        const views = viewsText ? parseInt(viewsText, 10) : null;
+
+        // Obtener nombre del canal
+        const channelName = $('meta[itemprop="author"]').attr('content') || $('link[itemprop="name"]').attr('content');
+
+        const onlinve = $('meta[itemprop="author"]').attr('content') || $('link[itemprop="name"]').attr('content');
+
+        console.log($('meta[itemprop="author"]'));
+
+        const videoData = {
+            title,
+            description,
+            views,
+            channelName,
+            onlinve
+        };
+
+        res.json(videoData);
     } catch (error) {
         console.error(error);
         res.status(500).send('Ocurrió un error al hacer el scraping');
